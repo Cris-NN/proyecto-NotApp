3a334a2c25dd9ee6e2919caa31c10e8e566f2357
Cristian <71856865+Cris-NN@users.noreply.github.com>
2025-10-22 19:31:50 -0300

    Add files via upload

---
+++ models/nota.js
@@ -0,0 +1,20 @@
+class nota {
+
+    constructor(titulo, texto, userId, id){
+        this.titulo = toString(titulo);
+        this.texto = toString(texto);
+        this.userId = toString(userId);
+        this.id = toString(id)
+    }
+
+    crearNota(){
+    }
+
+    modificarNota(){
+
+    }
+
+    eliminarNota(){
+        
+    }
+}
---
+++ models/user.js
@@ -0,0 +1,25 @@
+class user{
+    constructor(nombre, apellido, mail, password, id){
+        this.nombre = toString(nombre);
+        this.apellido = toString(apellido);
+        this.mail = toString(mail);
+        this.password = toString(password);
+        this.id = toString(id); 
+    }
+
+    crearUser(){
+        
+    }
+
+    verificarUser(){
+
+    }
+    
+    modificarUser(){
+
+    }
+
+    eliminarUser(){
+
+    }
+}
